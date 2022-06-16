943e6ba46d0cb01f549a12ab55d2f8262f2cf80a
Nathan Vegdahl <cessen@cessen.com>
2022-06-16 15:01:38 -0700

    Add operator for normalizing unlocked unselected weights.

--- __init__.py
+++ __init__.py
@@ -145,6 +145,92 @@ class NormalizeUnlockedDeformWeights(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class NormalizeUnlockedUnselectedDeformWeights(bpy.types.Operator):
+    """Normalizes unlocked deform weights in a way that prioritizes the selected weight groups remaining unchanged."""
+    bl_idname = "paint.normalize_unlocked_unselected_deform_weights"
+    bl_label = "Normalize Unlocked Unselected Deform Weights"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None and \
+               context.mode == 'PAINT_WEIGHT'
+
+    def execute(self, context):
+        obj = context.active_object
+
+        # Get the armature (if any) that deforms this object.
+        armature = None
+        for mod in obj.modifiers:
+            if mod.type == 'ARMATURE' and mod.object != None:
+                armature = mod.object
+                break
+        if armature is None:
+            return {'CANCELLED'}
+
+        # Get a list of deform bones from the deforming armature.
+        deform_bones = set()
+        deform_bones_selected = set()
+        for bone in armature.data.bones:
+            if bone.use_deform:
+                deform_bones.add(bone.name)
+                if bone.select:
+                    deform_bones_selected.add(bone.name)
+
+        # Build a list of vertex group indices for locked and unlocked deform groups,
+        # the latter being split into selected and unselected.
+        locked = []
+        unlocked_selected = []
+        unlocked_unselected = []
+        for group in obj.vertex_groups:
+            if group.name in deform_bones:
+                if group.lock_weight:
+                    locked += [group.index]
+                elif group.name in deform_bones_selected:
+                    unlocked_selected += [group.index]
+                else:
+                    unlocked_unselected += [group.index]
+
+        # Loop through the vertices and normalize the unlocked deform groups.
+        for vert in obj.data.vertices:
+            # Get the total locked and unlocked weights.
+            locked_sum = 0.0
+            unlocked_selected_sum = 0.0
+            unlocked_unselected_sum = 0.0
+            for group in vert.groups:
+                if group.group in locked:
+                    locked_sum += group.weight
+                elif group.group in unlocked_selected:
+                    unlocked_selected_sum += group.weight
+                elif group.group in unlocked_unselected:
+                    unlocked_unselected_sum += group.weight
+            if unlocked_selected_sum <= 0.0 and unlocked_unselected_sum <= 0.0:
+                continue
+
+            # Normalize.
+            if locked_sum >= 1.0:
+                for group in vert.groups:
+                    if group.group not in locked:
+                        group.weight = 0.0
+            else:
+                room = 1.0 - locked_sum
+                unselected_room = room - unlocked_selected_sum
+                norm_selected = 1.0
+                norm_unselected = 1.0
+                if unselected_room <= 0.0 or unlocked_unselected_sum <= 0.0:
+                    norm_selected = room / unlocked_selected_sum
+                    norm_unselected = 0.0
+                else:
+                    norm_selected = 1.0
+                    norm_unselected = unselected_room / unlocked_unselected_sum
+
+                for group in vert.groups:
+                    if group.group in unlocked_selected:
+                        group.weight *= norm_selected
+                    if group.group in unlocked_unselected:
+                        group.weight *= norm_unselected
+
+        return {'FINISHED'}
+
 
 #========================================================
 
@@ -153,11 +239,15 @@ def register():
     bpy.utils.register_class(NormalizeQuaternions)
     bpy.utils.register_class(ToggleAddSubWeightPaintBrush)
     bpy.utils.register_class(NormalizeUnlockedDeformWeights)
+    bpy.utils.register_class(NormalizeUnlockedUnselectedDeformWeights)
+
+
 
 def unregister():
     bpy.utils.unregister_class(NormalizeQuaternions)
     bpy.utils.unregister_class(ToggleAddSubWeightPaintBrush)
     bpy.utils.unregister_class(NormalizeUnlockedDeformWeights)
+    bpy.utils.unregister_class(NormalizeUnlockedUnselectedDeformWeights)
 
 
 if __name__ == "__main__":
